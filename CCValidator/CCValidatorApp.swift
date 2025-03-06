e01875712c642d2029ea19cad44d43418d3cbc54
PhilipKonyarov <philip.konyarov@hedgehoglab.com>
2025-03-06 09:32:27 +0200

    Pushing initial project files to the remote.

---
+++ CCValidator/CCValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCValidatorApp.swift
+//  CCValidator
+//
+//  Created by mac on 5.03.25.
+//
+
+import SwiftUI
+
+@main
+struct CCValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCValidator/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  CCValidator
+//
+//  Created by mac on 5.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            
+        }
+    }
+}
+ 
